bd421ec32732247d97398cceeb0739fc58654c19
QuinnPooch <pucciquinn@gmail.com>
2021-11-13 10:37:14 -0700

    added end game function

--- script.js
+++ script.js
@@ -2,8 +2,58 @@ var startButton = document.getElementById('start-btn')
 var questionContainerEl = document.getElementById('question-container')
 var questionEl = document.getElementById('question')
 var answerButtonsEl = document.getElementById('answer-btns')
+var choiceOne = document.getElementById('choice1')
+var choiceTwo = document.getElementById('choice2')
+var choiceThree = document.getElementById('choice3')
+var choiceFour = document.getElementById('choice4')
+var timer = 75
 
-var currentQuestionIndex = questions
+// question array
+
+var questions = [
+    {
+        question: "this is question 1",
+        answers: [
+            { text: 'possible answer a'},
+            { text: 'possible answer b'},
+            { text: 'possible answer c'},
+            { text: 'possible answer d'}
+        ],
+        correctAnswer: "possible answer a"
+    },
+    {
+        question: "this is question 2",
+        answers: [
+            { text: 'possible answer e'},
+            { text: 'possible answer f'},
+            { text: 'possible answer g'},
+            { text: 'possible answer h'}
+        ],
+        correctAnswer: "possible answer f"
+    },
+    {
+        question: "this is question 3",
+        answers: [
+            { text: 'possible answer i'},
+            { text: 'possible answer j'},
+            { text: 'possible answer k'},
+            { text: 'possible answer l'}
+        ],
+        correctAnswer: "possible answer l"
+    },
+    {
+        question: "this is question 4",
+        answers: [
+            { text: 'possible answer m'},
+            { text: 'possible answer n'},
+            { text: 'possible answer o'},
+            { text: 'possible answer p'}
+        ],
+        correctAnswer: "possible answer m"
+    }
+]
+
+var currentQuestionIndex = 0
 
 startButton.addEventListener('click', startGame)
 
@@ -14,57 +64,81 @@ startButton.addEventListener('click', startGame)
 // start game
 function startGame() {
 startButton.classList.add("hide")
-currentQuestionIndex = 0
 questionContainerEl.classList.remove('hide')
-stageQuestion()
-
-// something to start timer within this function
-
+document.getElementById("timer").innerText=timer
+timer = 75
+currentQuestionIndex = 0
+setInterval(countDown, 1000)
+showQuestion()
 }
 // start game end
 
 
-// stage question
-function stageQuestion() {
-    showQuestion(currentQuestionIndex)
-}
-// stage question end
-
 //  show current question
-function showQuestion(question) {
- questionEl.innerText = question.question
- question.answers.answers.forEach(answer => {
-   var button = document.createElement('button') 
-   button.innertext = answer.text
-   button.classList.add("btn")
-
-    // if statment for correct answer goes here
+function showQuestion() {
+    console.log(questionEl)
     
- })
+    questionEl.innerText = questions[currentQuestionIndex].question
 
+    choiceOne.innerText = questions[currentQuestionIndex].answers[0].text
+    choiceTwo.innerText = questions[currentQuestionIndex].answers[1].text
+    choiceThree.innerText = questions[currentQuestionIndex].answers[2].text
+    choiceFour.innerText = questions[currentQuestionIndex].answers[3].text
+
+    choiceOne.addEventListener("click", nextQuestion)
+    choiceTwo.addEventListener("click", nextQuestion)
+    choiceThree.addEventListener("click", nextQuestion)
+    choiceFour.addEventListener("click", nextQuestion)  
 }
+// show current question end
 
-// show current question
-
-// select answer
-function selectAnswer() {
-
+// next question
+function nextQuestion(event) {
+    event.preventDefault()
+    console.log(questions[currentQuestionIndex].correctAnswer)
+    if (questions[currentQuestionIndex].correctAnswer === event.target.innerHTML) {
+        console.log("correct")
+    }
+    else {
+        timer=timer-8 
+        document.getElementById("timer").innerText=timer
+        console.log("incorrect")
+    }
+    console.log(event.target.innerHTML)
+    currentQuestionIndex++
+    if (currentQuestionIndex === questions.length) {
+        console.log(questions.length)
+        endGame()
+    } 
+    else {
+    showQuestion()
+    }
+    
 }
-// select answer end
+// next question end
 
+// end game
+function endGame() {
+    var score = timer
+    window.prompt("your score is " + score)
+}
+// end game end
+
+// countdown
+function countDown() {
+    timer--
+    document.getElementById("timer").innerText=timer
+
+    if (timer <= 0) {
+     var retry = confirm('Game over, press "OK" to try again and "CANCEL" to view give up')
+        if (retry) {
+         startGame()
+        }
+    }
+      else {
+      // CALL END GAME FUNCTION HERE
+     }
+};
+// countdown end
 
 // ---------------------FUNCTIONS END------------------
-
-// question array
-
-var questions = [
-    {
-        question: "this is question 1",
-        answers: [
-            { text: 'possible answer a', correct: true},
-            { text: 'possible answer b', correct: false},
-            { text: 'possible answer c', correct: false},
-            { text: 'possible answer d', correct: false}
-        ]
-    }
-]
